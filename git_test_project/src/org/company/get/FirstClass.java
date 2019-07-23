9503ecb7097f00061b8e5717b0b91f2ecb1bf253
AD <136768641@qq.com>
2019-07-23 09:29:59 +0800

    从远程仓库下载后修改此工程内容

--- git_test_project/src/org/company/get/FirstClass.java
+++ git_test_project/src/org/company/get/FirstClass.java
@@ -7,6 +7,8 @@ public class FirstClass {
         System.out.println("111111111111111");
         System.out.println("111111111111111");
         System.out.println("111111111111111");
+        System.out.println("222222222222222");
+        System.out.println("2222222222222222222222");
     }
     public void test(){
         int i =0;
